371079bffcee7ac84b54ad60c40f3cffbf74915d
VladimirWithLove <130908809+VladimirWithLove@users.noreply.github.com>
2023-08-16 13:58:10 +0300

    Add files via upload

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,47 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user.getCar());
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> getUserByCarModelAndSeries(String model, int series) {
+      TypedQuery<Car> query = sessionFactory.getCurrentSession().createQuery("from Car " +
+              "where model = :model and series = :series");
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      List<Car> cars = query.getResultList();
+      List<User> result = new ArrayList<>();
+      for (Car car : cars) {
+         result.add(car.getUser());
+      }
+      return result;
+   }
+}
